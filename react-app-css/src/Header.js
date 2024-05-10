04fb7c2c485073ca18b52488342bf21a81808cd0
Vishal Rathod <vishalrathod7597@gmail.com>
2024-05-10 18:33:32 +0530

    new upload

---
+++ react-app-css/src/Header.js
@@ -0,0 +1,11 @@
+import logo from './logo.png';
+
+export default function Header() {
+  return (
+    <header>
+      <img src={logo} alt="A canvas" />
+      <h1>ReactArt</h1>
+      <p>A community of artists and art-lovers.</p>
+    </header>
+  );
+}
--- third-app/src/App.js
+++ third-app/src/App.js
@@ -21,21 +21,61 @@
 //
 // export default App;
 
+                                
+
+                            // 2nd task code //
+
+
+
+// import React from 'react';
+// import './App.css';
+
+// export default function App() {
+//     const [price, setPrice] = React.useState(100);
+
+//     function handleClick() {
+//         setPrice(75);
+//     }
+
+//     return (
+//         <div>
+//             <p data-testid="price">${price}</p>
+//             <button onClick={handleClick}>Apply Discount</button>
+//         </div>
+//     );
+// }
+
+
+
+
 import React from 'react';
-import './App.css';
-
-export default function App() {
-    const [price, setPrice] = React.useState(100);
-
-    function handleClick() {
-        setPrice(75);
-    }
-
-    return (
-        <div>
-            <p data-testid="price">${price}</p>
-            <button onClick={handleClick}>Apply Discount</button>
-        </div>
-    );
+// import { useState } from 'react';
+ import './App.css';
+ 
+function App() {
+  const [choice, setChoice] = React.useState(null);
+ 
+  let textColor = 'white';
+ 
+  if (choice === 'yes') {
+    textColor = 'green';
+  } else if (choice === 'no') {
+    textColor = 'red';
+  }
+ 
+  return (
+    <div id="app">
+      <h1 style={{ color: textColor }}>CSS is great!</h1>
+      <menu>
+        <li>
+          <button onClick={() => setChoice('yes')}>Yes</button>
+        </li>
+        <li>
+          <button onClick={() => setChoice('no')}>No</button>
+        </li>
+      </menu>
+    </div>
+  );
 }
+export default App;
 
