3539329521934cbb6ac8fb3c2cb3814e2598e025
Vishal Rathod <vishalrathod7597@gmail.com>
2024-05-13 16:58:42 +0530

    new uploads

--- fifth-app/src/App.js
+++ fifth-app/src/App.js
@@ -12,21 +12,75 @@
 
 // export default App;
 
-import React from 'react';
+
+                                //2nd app
+
+
+// import React from 'react';
  
-// don't change the Component name "App"
-export default function App() {
-    const [highlighted, setHighlighted] = React.useState(false);
+// // don't change the Component name "App"
+// export default function App() {
+//     const [highlighted, setHighlighted] = React.useState(false);
     
-    function clickHandler() {
-        setHighlighted(isHighlighted => !isHighlighted);
-    }
+//     function clickHandler() {
+//         setHighlighted(isHighlighted => !isHighlighted);
+//     }
     
-    return (
-        <div>
-            <p style={{color: highlighted ? 'red' : 'white'}}>Style me!</p>
-            <button onClick={clickHandler}>Toggle style</button>
-        </div>
-    );
+//     return (
+//         <div>
+//             <p style={{color: highlighted ? 'red' : 'white'}}>Style me!</p>
+//             <button onClick={clickHandler}>Toggle style</button>
+//         </div>
+//     );
+// }
+
+                                        /* 3rd app */
+
+
+//  import React from 'react';
+
+// import './App.css';
+
+// // don't change the Component name "App"
+// export default function App() {
+//     const clickHandler = () => {
+//         console.log('Clicked');
+//     };
+    
+//     return (
+//         <div>
+//           <h2>You're logged in</h2>
+//           <p>Welcome to your user profile!</p>
+//           <button onClick={clickHandler}>Click me</button>
+//         </div>
+//     );
+// }
+
+
+                                            /* 4th app */
+
+import React from 'react';
+import './App.css';
+ 
+function App() {
+  const filePicker = React.useRef();
+ 
+  function handleStartPickImage() {
+    filePicker.current.click();
+  }
+ 
+  return (
+    <div id="app">
+      <p>Please select an image</p>
+      <p>
+        <input data-testid="file-picker" type="file" accept="image/*" ref={filePicker} />
+        <button onClick={handleStartPickImage}>Pick Image</button>
+      </p>
+    </div>
+  );
 }
+export default App;
+
+                                    
+
 
---
+++ refs-portals/src/App.js
@@ -0,0 +1,19 @@
+import Player from "./Player.js"; 
+import TimerChallenge from "./TimerChallenge.js";
+
+function App() {
+  return (
+    <>
+      <Player />
+      <div id="challenges">
+        <TimerChallenge title= "Easy" targetTime={1} />
+        <TimerChallenge title= "Not Easy" targetTime={5} />
+        <TimerChallenge title= "Getting Tough" targetTime={10} />
+        <TimerChallenge title= "Pros only" targetTime={15} />
+
+      </div>
+    </>
+  );
+}
+
+export default App;
