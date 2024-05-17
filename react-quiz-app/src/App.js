1b7271cf4d6a09ebe46b9a337610cbedb7e8d9e1
Vishal Rathod <vishalrathod7597@gmail.com>
2024-05-17 18:44:31 +0530

    new project app uploaded

---
+++ react-quiz-app/src/App.js
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import Quiz from "./components/Quiz.js";
+
+
+
+
+function App() {
+  return(
+    <>
+    <Header />
+    <main>
+    <Quiz />
+    </main>
+    </>
+  );
+
+}
+
+export default App;
