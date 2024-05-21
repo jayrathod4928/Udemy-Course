10d3aeb3737be7c9b3a99efcbb7b9b29cad93d6f
Vishal Rathod <vishalrathod7597@gmail.com>
2024-05-21 17:30:00 +0530

    Input form uploads

---
+++ forms-user-input-app/src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Signup from "./components/Signup.js";
+// import Login from "./components/StateLogin.js";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        {/* <Login /> */}
+        <Signup />
+      </main>
+    </>
+  );
+}
+
+export default App;
